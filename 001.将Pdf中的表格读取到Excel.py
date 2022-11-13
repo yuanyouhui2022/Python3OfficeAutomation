7d3c7eed8382245158e5d1c624d5e0d4068d36b7
{xyfhy2004} <ljms8181@126.com>
2022-11-13 18:14:14 +0800

    初始化，并添加001demo

---
+++ 001.将Pdf中的表格读取到Excel.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Name:         001.将Pdf中的表格读取到Excel.py
+Description:  猿友汇技术团队出品
+Author:       老徐
+Date:         2022/10/29 5:36 PM
+Version:      V1.0
+"""
+import pdfplumber
+import pandas
+
+with pdfplumber.open("todofiles/2022年某价格表.pdf") as pdf:
+    page = pdf.pages[0]
+    table = page.extract_table()
+    temp = pandas.DataFrame(table[1:], columns=table[0])
+    temp.to_excel("test.xlsx")
+    print("ok")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if __name__ == '__main__':
+#     print('Hi, This is 001.将Pdf中的表格读取到Excel.py')
+#
